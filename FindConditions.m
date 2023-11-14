ea66fcf3170a329a31fa000ef988b28e92257b98
Samantha N Pitts <95096147+SamanthaNPitts@users.noreply.github.com>
2023-11-14 14:27:51 -0500

    Add files via upload

---
+++ FindConditions.m
@@ -0,0 +1,14 @@
+
+% 
+% a = 89978;
+% b = 812472;
+c = 8; %conditions in range 
+
+% range = [a,b];
+
+range = [3017,1722180];
+
+num_samples = range(2)-range(1)+1;
+
+starting_sample_condition = [range(1):ceil(num_samples/c):range(2)]
+
